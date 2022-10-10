f8c67b3dea6ae6e5d2e8416cca436f69f9115cf7
Tejaswa Yadav <tejaswayadav@Tejaswas-MacBook-Air.local>
2022-10-10 11:12:24 +0530

    Initial commit

---
+++ src/main/java/org/neko/poc/webfluxcontroller/WebfluxcontrollerApplication.java
@@ -0,0 +1,13 @@
+package org.neko.poc.webfluxcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxcontrollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/neko/poc/webfluxcontroller/WebfluxcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neko.poc.webfluxcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
